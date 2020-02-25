ccc7bc049c313b2a73e38b47435baf5119db70bd
ARAVIND JAGADEESAN <40851141+ARAVIND61198@users.noreply.github.com>
2020-02-25 14:00:48 +0530

    Add files via upload

---
+++ BigInt.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+	BigInteger b = new  BigInteger(in.next());
+
+	BigInteger b1 = new  BigInteger(in.next());
+
+
+	System.out.println(b.add(b1));
+
+	System.out.println(b.multiply(b1));
+
+	}
+
+}
